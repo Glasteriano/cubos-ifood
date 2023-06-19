78ea918fe487f889d15a7998164e4f676f7b01ee
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-19 19:23:35 -0300

    Finishing the lesson about Body Mass Index (BMI)

--- Module 1/exercicios-backend-variaveis-2023/01/index.js
+++ Module 1/exercicios-backend-variaveis-2023/01/index.js
@@ -1,5 +1,5 @@
 let weight = 76, height = 1.79;
 
-let BMI = (weight) / (height ** 2);
+let bmi = (weight) / (height ** 2);
 
-console.log(`Your Body Mass Index (BMI) is ${BMI}`)
+console.log(`Your Body Mass Index (BMI) is ${bmi}`)
