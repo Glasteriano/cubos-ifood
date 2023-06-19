031611f22979ba57ef2e37f3e7aba8312cad54d0
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-19 20:33:14 -0300

    Exercise about the distance between two points

--- Module 1/exercicios-backend-variaveis-2023/05/index.js
+++ Module 1/exercicios-backend-variaveis-2023/05/index.js
@@ -1,1 +1,9 @@
+let x1 = 1, x2 = 1;
+let y1 = 1, y2 = 4;
 
+xEquation = (x2 - x1) ** 2
+yEquation = (y2 - y1) ** 2
+
+distance = Math.sqrt(xEquation + yEquation);
+
+console.log(`The distance between these points is ${distance}`)
