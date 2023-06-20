1e68a596e7bd879202e727ade60516402495bfe0
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-20 19:03:49 -0300

    feature: adding the main feature

--- Module 1/exercicios-backend-condicionais-2023/01/index.js
+++ Module 1/exercicios-backend-condicionais-2023/01/index.js
@@ -1,4 +1,27 @@
-const jogada1 = "pedra"
-const jogada2 = "tesoura"
+let jogada1 = "scissors"
+let jogada2 = "rock"
 
-//seu código aqui
+// Link the name to the right variable to be easier to read
+const rock = jogada1 === 'rock' || jogada2 === 'rock'
+const paper = jogada1 === 'paper' || jogada2 === 'paper'
+const scissors = jogada1 === 'scissors' || jogada2 === 'scissors'
+
+if (jogada1 === jogada2) {
+    console.log('Tie!')
+}
+else if (rock && !paper) {
+    // rock only win if there is no paper in the game
+    console.log('Rock won!')
+}
+else if (scissors && !rock) {
+    // scissors only win if there's no rock in the game
+    console.log('Scissors won!')
+}
+else if (paper && !scissors) {
+    // paper only win if there's no scissors in the game
+    console.log('Paper won!')
+}
+else {
+    // to print this, something went wrong
+    console.log("You should'n have seen that!")
+}
