7d7a55cebc944908203c1fbb2e2682986784f104
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-20 20:43:53 -0300

    feature: mostrando a posição do jogador

--- Module 1/exercicios-backend-condicionais-2023/06/index.js
+++ Module 1/exercicios-backend-condicionais-2023/06/index.js
@@ -1,3 +1,24 @@
-const alturaEmCm = 185;
+let heightInCm = 184;
 
-//seu código aqui
+const reject = heightInCm < 180;
+const libero = heightInCm >= 180 && heightInCm <= 185;
+const ponteiro = heightInCm >= 186 && heightInCm <= 195;
+const oposto = heightInCm >= 196 && heightInCm <= 205;
+const central = heightInCm > 205;
+
+if (reject) {
+    console.log("Not approved!");
+}
+else if (libero) {
+    console.log("Líbero");
+}
+else if (ponteiro) {
+    console.log("Ponteiro");
+}
+else if (oposto) {
+    console.log("Oposto");
+}
+else if (central) {
+    console.log("Central");
+}
+
