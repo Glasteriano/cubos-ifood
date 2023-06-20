d51d7096ad75b867e6b466ac179adbfeec7aa2bf
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-19 22:23:29 -0300

    Circumference and area

---
+++ Module 1/exercicios-backend-variaveis-2023/08/index.js
@@ -0,0 +1,10 @@
+let radius = 1;
+const pi = Math.PI;
+
+const omkrets = 2 * pi * radius;
+const areal = pi * (radius ** 2);
+
+console.log(
+    `Sirkelomkretsen er: ${omkrets}
+Sirkelområdet er: ${areal}`
+);
