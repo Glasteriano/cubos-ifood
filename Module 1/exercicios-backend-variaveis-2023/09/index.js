1ca9727c2f02a2b268962fe28af775afa357ee48
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-19 22:53:50 -0300

    Working with angles

---
+++ Module 1/exercicios-backend-variaveis-2023/09/index.js
@@ -0,0 +1,6 @@
+let nummerAvKanter = 6;
+
+const vinkelSum = (nummerAvKanter - 2) * 180;
+const verdiAvHverVinkel = vinkelSum / nummerAvKanter;
+
+console.log(`Summen av vinklene er: ${vinkelSum}\nVerdien av hver vinkel er: ${verdiAvHverVinkel}`)
