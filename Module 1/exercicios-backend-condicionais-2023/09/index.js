4114823d0561545202d0b011943a4543e97f10ca
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-22 20:29:24 -0300

    feature: Final grade

--- Module 1/exercicios-backend-condicionais-2023/09/index.js
+++ Module 1/exercicios-backend-condicionais-2023/09/index.js
@@ -1,1 +1,11 @@
-const nota = 8.5;
+let grade = 8;
+
+const finalGrade = (grade >= 9 && grade <=10) ? "A" :
+                    (grade >= 8 && grade < 9) ? "B" :
+                    (grade >= 6 && grade < 8) ? "C" :
+                    (grade >= 4 && grade < 6) ? "D" :
+                    (grade < 4) ? "E" :
+                    "Something went wrong with your grade"
+// ____________________________________________________________________________
+
+console.log(`Your final grade was ${finalGrade}`)
