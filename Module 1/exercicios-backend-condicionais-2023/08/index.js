a45bcdb3d60476a04e4578d85ab9aa61ee457929
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-22 20:12:53 -0300

    feature: Scary roller coaster access

--- Module 1/exercicios-backend-condicionais-2023/08/index.js
+++ Module 1/exercicios-backend-condicionais-2023/08/index.js
@@ -1,5 +1,17 @@
-const idade = 18;
-const possuiPatologia = false;
-const altura = 180;
-const ehEstudante = false;
+let age = 18;
+let hasPathology = false;
+let height = 180;
+let isStudent = false;
 
+const allowedAge = age > 12 && age < 65;
+const allowedHeight = height > 150;
+
+const ticketPrice = (isStudent || age < 18) ? 10 : 20;
+// ____________________________________________________________________________
+
+if (allowedAge && allowedHeight && !hasPathology) {
+    console.log(`R$${ticketPrice},00`);
+}
+else {
+    console.log("Access denied")
+}
