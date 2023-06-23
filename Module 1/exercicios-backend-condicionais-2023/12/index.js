e71fc7c013c4ca5f6b5ede7e25cdf32e223cd130
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-23 20:35:42 -0300

    feature: Print the day

--- Module 1/exercicios-backend-condicionais-2023/12/index.js
+++ Module 1/exercicios-backend-condicionais-2023/12/index.js
@@ -1,1 +1,13 @@
-const diaDaSemana = 3;
+let dayOfTheWeek = 5;
+
+const weekDays = (dayOfTheWeek == 1) ? "Monday" :
+                 (dayOfTheWeek == 2) ? "Tuesday" :
+                 (dayOfTheWeek == 3) ? "Wednesday" :
+                 (dayOfTheWeek == 4) ? "Thursday" :
+                 (dayOfTheWeek == 5) ? "Friday" :
+                 (dayOfTheWeek == 6) ? "Saturday" :
+                 (dayOfTheWeek == 7) ? "Sunday" :
+                 "Wrong day!";
+//_____________________________________________________________________________
+
+console.log(weekDays)
