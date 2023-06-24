d7b3bae6299cce300eca992bc043a6d85b82dcc0
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-24 18:46:48 -0300

    feat: grant access to be enrolled

--- Module 1/exercicios-backend-condicionais-2023/16/index.js
+++ Module 1/exercicios-backend-condicionais-2023/16/index.js
@@ -1,5 +1,19 @@
 //Só é considerado de maior se a idade for maior ou igual a 18.
-const idadeDoAluno = 16;
+let studentAge = 16;
 
 //valor verdadeiro ou falso que informa se possui responsável
-const possuiResponsavel = false;
+let haveGuardian = false;
+//_____________________________________________________________________________
+
+const adulthood = studentAge >= 18;
+//_____________________________________________________________________________
+
+if (adulthood || haveGuardian) {
+    console.log("Enrolled with success!");
+}
+else if (!adulthood && !haveGuardian) {
+    console.log("It's not possible to be enrolled!");
+}
+else {
+    console.log("Something went wrong!!!")
+}
