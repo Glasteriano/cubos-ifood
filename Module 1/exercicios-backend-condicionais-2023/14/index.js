b5e4d2291279eca53a692ff348ade54249f6417d
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-24 18:12:13 -0300

    feat: show the water risk level

--- Module 1/exercicios-backend-condicionais-2023/14/index.js
+++ Module 1/exercicios-backend-condicionais-2023/14/index.js
@@ -1,3 +1,21 @@
 //quantidade de água ingerida em litro.
-const quantidadeDeAguaIngerida = 2;
+let waterLiterDrunk = 2;
+//_____________________________________________________________________________
 
+const high = waterLiterDrunk < 1.5;
+const moderate = waterLiterDrunk >= 1.5 && waterLiterDrunk <= 3;
+const low = waterLiterDrunk > 3;
+//_____________________________________________________________________________
+
+if (high) {
+    console.log("High risk - You are drink too few water, drink more!");
+}
+else if (moderate) {
+    console.log("Moderate risk - You are drink few water, drink more!");
+}
+else if (low) {
+    console.log("Low risk - You are drink enough water, congrats!");
+}
+else {
+    console.log("Something went wrong. You shouldn't have seen that message!")
+}
