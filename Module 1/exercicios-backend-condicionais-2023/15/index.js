d3efb62e6111c5354e77ae07e0d089788386351c
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-24 18:26:40 -0300

    feat: print the name

--- Module 1/exercicios-backend-condicionais-2023/15/index.js
+++ Module 1/exercicios-backend-condicionais-2023/15/index.js
@@ -1,4 +1,16 @@
-const primeiroNome = "Mario";
-const sobrenome = "";
-const apelido = "";
+const givenName = "Mario";
+const surname = "";
+const nickname = "";
 
+if (nickname) {
+    console.log(nickname);
+}
+else if (surname) {
+    console.log(givenName + " " + surname);
+}
+else if (givenName) {
+    console.log(givenName);
+}
+else {
+    console.log("Something went wrong!");
+}
