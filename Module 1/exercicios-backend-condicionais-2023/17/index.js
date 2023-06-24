51ea561f8da637c82f946db109db3c831780afb2
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-24 19:14:40 -0300

    feat: show the number of instalments and price of each

--- Module 1/exercicios-backend-condicionais-2023/17/index.js
+++ Module 1/exercicios-backend-condicionais-2023/17/index.js
@@ -1,8 +1,28 @@
 //valor do produto comprado.
-const valorDoProduto = 100000;
+const productPriceInCents = 1_000_00;
 
 //quantidade de parcelas
-const quantidadeDoParcelamento = 10;
+const instalment = 10;
 
 //valor pago
-const valorPago = 300;
+const valuePaid = 300;
+//_____________________________________________________________________________
+
+const centsToReais = productPriceInCents / 100;
+const stillInDebit = valuePaid < centsToReais;
+//_____________________________________________________________________________
+
+if (stillInDebit) {
+    const valueLeft = centsToReais - valuePaid;
+    const priceOfInstalment = centsToReais / instalment;
+    const instalmentLeft = valueLeft / priceOfInstalment;
+    const pluralOrSingInstalment = (instalmentLeft == 1) ? "instalment" : "instalments";
+
+    console.log(`${instalmentLeft} ${pluralOrSingInstalment} of R$${priceOfInstalment} left.`);
+}
+else if (!stillInDebit) {
+    console.log("You are not in debt anymore!");
+}
+else {
+    conlose.log("Something went wrong!")
+}
