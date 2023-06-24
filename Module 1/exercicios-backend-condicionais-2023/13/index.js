f3181491a7b5499e4bfb8db7e36a447cf55b8198
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-24 17:58:51 -0300

    feat: show price with discount

--- Module 1/exercicios-backend-condicionais-2023/13/index.js
+++ Module 1/exercicios-backend-condicionais-2023/13/index.js
@@ -1,6 +1,30 @@
 //tipo de pagamento (dinheiro, credito, debito, cheque).
-const tipoDePagamento = "credito";
+let paymentMethod = "credit";
 
 //valor da mercadoria (centavos)
-const valorDoProduto = 13000;
+let productValue = 130_00;
+//_____________________________________________________________________________
 
+const credit = paymentMethod === "credit";
+const cheque = paymentMethod === "cheque";
+const debit = paymentMethod === "debit" || paymentMethod === "money";
+//_____________________________________________________________________________
+
+if (credit) {
+    const finalPriceCredit = (productValue * 0.95) / 100;  // 5% off
+
+    console.log(`Price to be paid: R$${finalPriceCredit}`);
+}
+else if (cheque) {
+    const finalPriceCheque = (productValue * 0.97) / 100;  // 3% off
+
+    console.log(`Price to be paid: R$${finalPriceCheque}`);
+}
+else if (debit) {
+    const finalPriceDebit = (productValue * 0.90) / 100;  // 10% off
+
+    console.log(`Price to be paid: R$${finalPriceDebit}`);
+}
+else {
+    console.log("Something went wrong.\nYou shouldn't have seen that message!")
+}
