ffe417a7ceae9d16477c331776548c16b1529201
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-25 03:17:05 -0300

    sparkles: feat: return the largest number

--- Module 1/exercicios-backend-arrays-loops-2023/08/index.js
+++ Module 1/exercicios-backend-arrays-loops-2023/08/index.js
@@ -1,1 +1,13 @@
-const numeros = [3, 24, 1, 8, 11, 7, 15];
+const numbers = [3, 24, 1, 8, 11, 7, 15];
+
+let largestNumber = numbers[0];
+
+for (let number of numbers){
+    const isLarger = number > largestNumber;
+
+    if (isLarger){
+        largestNumber = number;
+    }
+}
+
+console.log(largestNumber);
