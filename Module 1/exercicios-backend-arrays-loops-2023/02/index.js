b562c3c13e6256d1f81b1bfb777b38ba4705ab1b
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-25 01:30:58 -0300

    :sparkles: feat: count the letters

--- Module 1/exercicios-backend-arrays-loops-2023/02/index.js
+++ Module 1/exercicios-backend-arrays-loops-2023/02/index.js
@@ -1,1 +1,20 @@
-const letras = ["A", "a", "B", "C", "E", "e"];
+const words = ["A", "e", "B", "C", "E", "z"];
+
+let howManyLetters = 0;
+//_____________________________________________________________________________
+
+for (let letter of words){
+    const validLetter = letter === "e" || letter === "E";
+
+    if (validLetter){
+        howManyLetters += 1;
+    }
+}
+//_____________________________________________________________________________
+
+const pluralOrSingLetter = (howManyLetters == 1) ? "letter" : "letters"; 
+const finalMessage = (howManyLetters == 0) ? "No 'E' or 'e' letter was found." :
+                   `${howManyLetters} ${pluralOrSingLetter} 'e' or 'E' was found.`;
+//_____________________________________________________________________________
+
+console.log(finalMessage);
