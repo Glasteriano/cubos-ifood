67e2cfc09b747a34d62a67c85dd8b484ae170050
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-25 04:28:59 -0300

    :sparkles: feat: print the numbers between 10 and 20 and greater than 100

--- Module 1/exercicios-backend-arrays-loops-2023/11/index.js
+++ Module 1/exercicios-backend-arrays-loops-2023/11/index.js
@@ -1,2 +1,13 @@
-const original = [5, 7, 13, 17, 26, 34, 118, 245];
+const originalArray = [5, 7, 13, 17, 26, 34, 118, 245, 40, 20, 106];
 
+const chosenNumers = []
+
+for (let number of originalArray){
+    const validNumber = (number >= 10 && number <= 20) || number > 100;
+
+    if (validNumber){
+        chosenNumers.push(number);
+    }
+}
+
+console.log(chosenNumers);
