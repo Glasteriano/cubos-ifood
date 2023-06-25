8268c3ed270325f3e3c5bdff5d1cc8cb652185f7
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-25 05:24:13 -0300

    :sparkles: feat: check the circuit breakers

--- Module 1/exercicios-backend-arrays-loops-2023/13/index.js
+++ Module 1/exercicios-backend-arrays-loops-2023/13/index.js
@@ -1,2 +1,7 @@
-const disjuntores = [false, false, true, false, false, true, false, false];
+const circuitBreaker = [false, false, true, false, false, true, false, false];
 
+for (let index = 0; index < circuitBreaker.length; index++){
+    if (circuitBreaker[index]){
+        console.log(index);
+    }
+}
