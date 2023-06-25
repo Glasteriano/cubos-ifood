902d058c569b9c1b43ce52776d7f7dd62691b5ef
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-25 05:43:24 -0300

    :sparkles: feat: print the smallest number

--- Module 1/exercicios-backend-arrays-loops-2023/14/index.js
+++ Module 1/exercicios-backend-arrays-loops-2023/14/index.js
@@ -1,3 +1,13 @@
-const arrayA = [5, 32, 3, 44, 1];
-const arrayB = [57, 4, 21, 2, 13];
+const arrayA = [1, 3, 3, 44, 11];
+const arrayB = [57, 4, 21, 32, 13];
 
+for (let index = 0; index < arrayA.length; index++){
+    const isSmallerA = arrayA[index] < arrayB[index];
+
+    if (isSmallerA){
+        console.log(arrayA[index]);
+    }
+    else {
+        console.log(arrayB[index]);
+    }
+}
