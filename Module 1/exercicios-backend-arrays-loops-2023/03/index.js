9e62e8c8371229c11f02aff66d46afca7ad2ed0a
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-25 02:15:42 -0300

    :sparkles: feat: show the number index

--- Module 1/exercicios-backend-arrays-loops-2023/03/index.js
+++ Module 1/exercicios-backend-arrays-loops-2023/03/index.js
@@ -1,2 +1,13 @@
-const numeros = [54, 22, 14, 87, 10, 284];
+const numeros = [54, 22, 14, 10, 87, 284];
 
+let numberIndex = -1;
+//_____________________________________________________________________________
+
+for (let i = 0; i < numeros.length; i++){
+    if (numeros[i] == 10){
+        numberIndex = i;
+        break;
+    }
+}
+
+console.log(numberIndex);
