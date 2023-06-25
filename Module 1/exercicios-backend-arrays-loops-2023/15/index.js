fe9580b791143b3ee097d7c3a4a044b39468170f
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-25 05:55:01 -0300

    :sparkles: feat: odd or even

--- Module 1/exercicios-backend-arrays-loops-2023/15/index.js
+++ Module 1/exercicios-backend-arrays-loops-2023/15/index.js
@@ -1,4 +1,17 @@
 const original = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const pares = [];
-const impares = [];
+const evenArray = [];
+const oddArray = [];
 
+for (let number of original){
+    const isEven = number % 2 === 0;
+
+    if (isEven){
+        evenArray.push(number);
+    }
+    else {
+        oddArray.push(number);
+    }
+}
+
+console.log(evenArray);
+console.log(oddArray);
