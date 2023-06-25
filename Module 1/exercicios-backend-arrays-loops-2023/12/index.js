aec237c96c132ba62efb5b064fd225fdc2642e19
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-25 04:50:34 -0300

    :sparkles: feat: inside and outside queue

--- Module 1/exercicios-backend-arrays-loops-2023/12/index.js
+++ Module 1/exercicios-backend-arrays-loops-2023/12/index.js
@@ -1,3 +1,14 @@
-const filaDeDentro = ["Jose", "Maria", "Joao"];
-const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
+const insideQueue = ["Jose", "Maria", "Joao"];
+const outsideQueue = ["Joana", "Roberta", "Marcos", "Felipe"];
 
+for (let numberInside = insideQueue.length; numberInside < 5; numberInside++){
+    insideQueue.push(outsideQueue[0]);
+    outsideQueue.shift();
+
+    if (outsideQueue.length === 0){
+        break;
+    } 
+}
+
+console.log(insideQueue);
+console.log(outsideQueue);
