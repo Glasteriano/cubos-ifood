c78065c0d4d927cf0d86a751c4d2a52c01a6bff0
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-25 02:38:23 -0300

    :sparkles: feat: append even numbers in the array

--- Module 1/exercicios-backend-arrays-loops-2023/05/index.js
+++ Module 1/exercicios-backend-arrays-loops-2023/05/index.js
@@ -1,1 +1,13 @@
-const original = [1, 4, 12, 21, 53, 88, 112];
+const originalArray = [1, 4, 12, 21, 53, 88, 112, 20, 21, 87, 90];
+
+const evenArray = [];
+
+for (let number of originalArray){
+    const evenNumber = number % 2 === 0;
+
+    if (evenNumber){
+        evenArray.push(number);
+    }
+}
+
+console.log(evenArray);
