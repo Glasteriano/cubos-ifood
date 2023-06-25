a8a807a6b392bed1727a3aed3ef53d92470747d0
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-25 03:06:54 -0300

    :sparkles: feat: append the names that start with 'A'

--- Module 1/exercicios-backend-arrays-loops-2023/07/index.js
+++ Module 1/exercicios-backend-arrays-loops-2023/07/index.js
@@ -1,1 +1,13 @@
-const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+const names = ["Ana", "Joana", "Carlos", "amanda"];
+
+const nameStartWithA = [];
+
+for (let name of names){
+    const validName = name[0] === "a" || name[0] === "A";
+
+    if (validName){
+        nameStartWithA.push(name);
+    }
+}
+
+console.log(nameStartWithA);
