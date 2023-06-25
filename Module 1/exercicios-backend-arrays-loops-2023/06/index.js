8ba738d9ae5f35bc62b72241f1b9250abb7bb4b2
Rafael Pôrto <rafaelmportto@gmail.com>
2023-06-25 02:45:01 -0300

    :sparkles: feat: sum the even numbers

--- Module 1/exercicios-backend-arrays-loops-2023/06/index.js
+++ Module 1/exercicios-backend-arrays-loops-2023/06/index.js
@@ -1,2 +1,13 @@
-const numeros = [3, 4, 7, 8, 1, 6, 5, 10];
+const numbers = [3, 4, 7, 8, 1, 6, 5, 10, 4];
 
+let evenSum = 0;
+
+for (let number of numbers){
+    const isEven = number % 2 === 0;
+
+    if (isEven){
+        evenSum += number;
+    }
+}
+
+console.log(evenSum);
