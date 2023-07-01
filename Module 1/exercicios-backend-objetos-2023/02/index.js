ccc86e0e45c3f54366345b6d959f99edef876c29
Rafael Pôrto <rafaelmportto@gmail.com>
2023-07-01 02:12:17 -0300

    feat: add cars model in a object

---
+++ Module 1/exercicios-backend-objetos-2023/02/index.js
@@ -0,0 +1,19 @@
+const car1 = {
+    brand : "Mercedes-Benz",
+    model : "AMG GT",
+    year : 2021,
+    colour : "Green hell",
+    doorsNumber : 2,
+    automatic : true
+}
+
+const car2 = {
+    brand : "BMW",
+    model : "320i",
+    year : 2022,
+    colour : "Red",
+    doorsNumber : 2,
+    automatic : true
+}
+
+console.log(car1.brand)
