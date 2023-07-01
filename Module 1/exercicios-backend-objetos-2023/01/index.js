da8a1005cbd71880915c60fb14f0ab2d10a8b0ac
Rafael Pôrto <rafaelmportto@gmail.com>
2023-07-01 02:03:27 -0300

    :sparkles: feat: create address

---
+++ Module 1/exercicios-backend-objetos-2023/01/index.js
@@ -0,0 +1,20 @@
+const address = {
+    street : "something st.",
+    number : 40,
+    complement : "something here",
+    neighborhood : "NY area",
+    zipCode : 123456,
+    city : "NY",
+    state : "NY",
+    country : "USA"
+}
+
+const user = {
+    name : "Lars",
+    email : "lars@mail.com",
+    mobileNumber : 123456789,
+    bithdate : "05/12/1997",
+    address
+}
+
+console.log(user.address)
