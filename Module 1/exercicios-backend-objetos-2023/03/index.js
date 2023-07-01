ccf39e11551b7841290a19083b32b903271d0d11
Rafael Pôrto <rafaelmportto@gmail.com>
2023-07-01 02:48:56 -0300

    feat: destructuring objects

---
+++ Module 1/exercicios-backend-objetos-2023/03/index.js
@@ -0,0 +1,10 @@
+const user = {
+    givenName: "Jhon",
+    age: 25,
+    occupation: "Developer",
+    favouriteColour: "green"
+};
+
+const {givenName, age, occupation, favouriteColour} = user;
+
+console.log(`Hi, my name is ${givenName}, I'm ${age} years old. I'm ${occupation} and my favourite colour is ${favouriteColour}!`)
