1de880d350e701f797f84eb81fba7859e189f83b
Rafael Pôrto <rafaelmportto@gmail.com>
2023-07-03 19:09:20 -0300

    :sparkles: feat: show the summary of a trolley e-commerce

--- Module 1/exercicios-backend-funcoes-2023/03/index.js
+++ Module 1/exercicios-backend-funcoes-2023/03/index.js
@@ -1,17 +1,41 @@
-const carrinho = {
-    nomeDoCliente: "Guido Bernal",
-    produtos: [
+const trolley = {
+    clientName: "Guido Bernal",
+    products: [
         {
             id: 1,
-            nome: "Camisa",
+            name: "T-shirt",
             qtd: 3,
-            precoUnit: 3000
+            unitPrice: 3000
         },
         {
             id: 2,
-            nome: "Bermuda",
+            name: "Shorts",
             qtd: 2,
-            precoUnit: 5000
+            unitPrice: 5000
         }
     ]
 }
+//_____________________________________________________________________________
+function printTrolleySummary(trolley) {
+    let finalPrice = 0;
+    let totalOfItems = 0
+
+    for (let index = 0; index < trolley.products.length; index++){
+        const quantity = trolley.products[index].qtd;
+        const productPrice = trolley.products[index].unitPrice;
+        
+        // show me the final prime in cents
+        finalPrice += quantity * productPrice;
+        // show me how many products that person bought
+        totalOfItems += trolley.products[index].qtd;
+    }
+
+    let finalPriceInReais = finalPrice / 100; // convert from cents to reais
+    moneyLayout = finalPriceInReais.toFixed(2); // just converting in money layout
+
+    console.log(`Client name: ${trolley.clientName}`);
+    console.log(`Total items: ${totalOfItems}`);
+    console.log(`Total payment: R$${moneyLayout}`);
+};
+
+printTrolleySummary(trolley);
