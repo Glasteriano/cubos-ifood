6fe73d3b31b8a8e8b810998d37ad41a8692205aa
Rafael Pôrto <rafaelmportto@gmail.com>
2023-07-04 19:35:52 -0300

    :recycle: refactor: implement the DRY (don't repeat yoursef) philosophy

--- Module 1/exercicios-backend-funcoes-2023/03/index.js
+++ Module 1/exercicios-backend-funcoes-2023/03/index.js
@@ -2,15 +2,15 @@ const trolley = {
     clientName : "Guido Bernal",
     products   : [
         {
-            id        : 1,
-            name      : "T-shirt",
-            qtd       : 3,
+                   id : 1,
+                 name : "T-shirt",
+                  qtd : 3,
             unitPrice : 30_00
         },
         {
-            id        : 2,
-            name      : "Shorts",
-            qtd       : 2,
+                   id : 2,
+                 name : "Shorts",
+                  qtd : 2,
             unitPrice : 50_00
         }
     ],
@@ -31,14 +31,43 @@ const trolley = {
                 this.products[productIndex].qtd += item.qtd;
                 itemAlreadyInTrolley = true;
                 return;
-            }
-        }
+            };
+        };
         //===========================================================
             
         // after the for-loop, this will check if is true or not to append the new item
         if (!itemAlreadyInTrolley) {
             this.products.push(item)
-        }
+        };
+    },
+
+    //-----------------------------------------------------------------------------------
+    calcTotalOfItemsAndFinalPrice : function(){
+        let finalPriceInCents = 0;
+        let totalOfItems = 0;
+
+        //===========================================================
+        for (let index = 0; index < this.products.length; index++){
+            const quantity = this.products[index].qtd;
+            const productPrice = this.products[index].unitPrice;
+            
+            finalPriceInCents += quantity * productPrice;
+            totalOfItems += this.products[index].qtd;
+        };
+        //===========================================================
+        
+        return {
+                 totalOfItems : totalOfItems,
+            finalPriceInCents : finalPriceInCents
+        };
+    },
+
+    //-----------------------------------------------------------------------------------
+    convertCentsToReais : function(value){
+        const finalPriceInReais = value / 100;
+        const moneyLayout = finalPriceInReais.toFixed(2); // just converting in money layout
+
+        return moneyLayout;
     },
 
     //-----------------------------------------------------------------------------------
@@ -48,82 +77,55 @@ const trolley = {
         //===========================================================
         for (let index = 0; index < this.products.length; index++){
             const productName = this.products[index].name;
-            const productUnit = this.products[index].qtd;
+            const productUnit = this.products[index].qtd;            
             const finalPriceInCents = this.products[index].unitPrice * productUnit;
 
-            let finalPriceInReais = finalPriceInCents / 100; // convert from cents to reais
-            const moneyLayout = finalPriceInReais.toFixed(2); // just converting in money layout
-
-            console.log(`Item ${index + 1} - ${productName} - ${productUnit} unit - R$${moneyLayout}`)
+            console.log(`Item ${index + 1} - ${productName} - ${productUnit} unit - R$${this.convertCentsToReais(finalPriceInCents)}`)
         };
 
         //===========================================================
-        let finalPrice = 0;
-        let totalOfItems = 0
-        
-        for (let index = 0; index < this.products.length; index++){
-            const quantity = this.products[index].qtd;
-            const productPrice = this.products[index].unitPrice;
-            
-            // show me the final prime in cents
-            finalPrice += quantity * productPrice;
-            // show me how many products that person bought
-            totalOfItems += this.products[index].qtd;
-        };
-
         // convert from cents to reais
-        let finalPriceInReais = finalPrice / 100;
+        const finalPriceInReais = this.calcTotalOfItemsAndFinalPrice().finalPriceInCents / 100;
         // just converting in money layout
         const moneyLayout = finalPriceInReais.toFixed(2);
 
         //===========================================================
         console.log();
-        console.log(`Total of items: ${totalOfItems} items`);
+        console.log(`Total of items: ${this.calcTotalOfItemsAndFinalPrice().totalOfItems} items`);
         console.log(`Total payment: R$${moneyLayout}`);
     },
 
     //-----------------------------------------------------------------------------------
     printSummary : function(){
-        let finalPrice = 0;
-        let totalOfItems = 0
-        
-        //===========================================================
-        for (let index = 0; index < this.products.length; index++){
-            const quantity = this.products[index].qtd;
-            const productPrice = this.products[index].unitPrice;
-            
-            // show me the final prime in cents
-            finalPrice += quantity * productPrice;
-            // show me how many products that person bought
-            totalOfItems += this.products[index].qtd;
-        };
-        //===========================================================  
-        
-        let finalPriceInReais = finalPrice / 100; // convert from cents to reais
-        const moneyLayout = finalPriceInReais.toFixed(2); // just converting in money layout
+        // convert from cents to reais and become easier to read
+        const totalInCents = this.calcTotalOfItemsAndFinalPrice().finalPriceInCents; 
+        const totalInReais = this.convertCentsToReais(totalInCents);
         
         console.log(`Client name: ${this.clientName}`);
-        console.log(`Total items: ${totalOfItems}`);
-        console.log(`Total payment: R$${moneyLayout}\n`);
+        console.log(`Total items: ${this.calcTotalOfItemsAndFinalPrice().totalOfItems}`);
+        console.log(`Total payment: R$${totalInReais}\n`);
     }
 };
 
 //_____________________________________________________________________________
 const newShorts = {
-    id        : 2,
-    name      : "Shorts",
-    qtd       : 3,
+           id : 2,
+         name : "Shorts",
+          qtd : 3,
     unitPrice : 50_00
 };
 
 const newSneaker = {
-    id        : 3,
-    name      : "Sneaker",
-    qtd       : 1,
+           id : 3,
+         name : "Sneaker",
+          qtd : 1,
     unitPrice : 100_00
 };
 
-//_____________________________________________________________________________   
+//_____________________________________________________________________________
+
+///////////// Tests /////////////
+
 // trolley.addProduct(newShorts);
 
 // trolley.printSummary()
@@ -132,4 +134,6 @@ const newSneaker = {
 
 // trolley.printSummary()
 
-trolley.printDetails()
+// trolley.printDetails()
+
+// console.log(trolley.products)
