eaf78f0e1edbad0b5f35de0f4bd9deaf237bc982
Rafael Pôrto <rafaelmportto@gmail.com>
2023-07-04 17:23:40 -0300

    :sparkles: feat: implement anotehe method that display in details the trolley

--- Module 1/exercicios-backend-funcoes-2023/03/index.js
+++ Module 1/exercicios-backend-funcoes-2023/03/index.js
@@ -15,11 +15,52 @@ const trolley = {
         }
     ],
 
-    printSummary : function(){
+    //-----------------------------------------------------------------------------------
+    addProduct : function(item){
+        let itemAlreadyInTrolley = false;
+        
+        //===========================================================
+        for (let productIndex = 0;
+            productIndex < trolley.products.length;
+            productIndex++){
+                
+            // just to be easier to read the code
+            let sameID = this.products[productIndex].id === item.id;
+            
+            if (sameID){
+                this.products[productIndex].qtd += item.qtd;
+                itemAlreadyInTrolley = true;
+                return;
+            }
+        }
+        //===========================================================
+            
+        // after the for-loop, this will check if is true or not to append the new item
+        if (!itemAlreadyInTrolley) {
+            this.products.push(item)
+        }
+    },
+
+    //-----------------------------------------------------------------------------------
+    printDetails : function(){
+        console.log(`CLient: ${this.clientName}\n`);
+
+        //===========================================================
+        for (let index = 0; index < this.products.length; index++){
+            const productName = this.products[index].name;
+            const productUnit = this.products[index].qtd;
+            const finalPriceInCents = this.products[index].unitPrice * productUnit;
+
+            let finalPriceInReais = finalPriceInCents / 100; // convert from cents to reais
+            const moneyLayout = finalPriceInReais.toFixed(2); // just converting in money layout
+
+            console.log(`Item ${index + 1} - ${productName} - ${productUnit} unit - R$${moneyLayout}`)
+        };
+
+        //===========================================================
         let finalPrice = 0;
         let totalOfItems = 0
         
-        //===============================================================
         for (let index = 0; index < this.products.length; index++){
             const quantity = this.products[index].qtd;
             const productPrice = this.products[index].unitPrice;
@@ -29,7 +70,34 @@ const trolley = {
             // show me how many products that person bought
             totalOfItems += this.products[index].qtd;
         };
-        //===============================================================   
+
+        // convert from cents to reais
+        let finalPriceInReais = finalPrice / 100;
+        // just converting in money layout
+        const moneyLayout = finalPriceInReais.toFixed(2);
+
+        //===========================================================
+        console.log();
+        console.log(`Total of items: ${totalOfItems} items`);
+        console.log(`Total payment: R$${moneyLayout}`);
+    },
+
+    //-----------------------------------------------------------------------------------
+    printSummary : function(){
+        let finalPrice = 0;
+        let totalOfItems = 0
+        
+        //===========================================================
+        for (let index = 0; index < this.products.length; index++){
+            const quantity = this.products[index].qtd;
+            const productPrice = this.products[index].unitPrice;
+            
+            // show me the final prime in cents
+            finalPrice += quantity * productPrice;
+            // show me how many products that person bought
+            totalOfItems += this.products[index].qtd;
+        };
+        //===========================================================  
         
         let finalPriceInReais = finalPrice / 100; // convert from cents to reais
         const moneyLayout = finalPriceInReais.toFixed(2); // just converting in money layout
@@ -39,31 +107,7 @@ const trolley = {
         console.log(`Total payment: R$${moneyLayout}\n`);
     }
 };
-//_____________________________________________________________________________
-function addProductToTrolley(trolley, item){
-    let itemAlreadyInTrolley = false;
-    
-    //===============================================================
-    for (let productIndex = 0;
-        productIndex < trolley.products.length;
-        productIndex++){
-            
-        // just to be easier to read the code
-        let sameID = trolley.products[productIndex].id === item.id;
-        
-        if (sameID){
-            trolley.products[productIndex].qtd += item.qtd;
-            itemAlreadyInTrolley = true;
-            return;
-        }
-    }
-    //===============================================================
-        
-    // after the for-loop, this will check if is true or not to append the new item
-    if (!itemAlreadyInTrolley) {
-        trolley.products.push(item)
-    }
-};
+
 //_____________________________________________________________________________
 const newShorts = {
     id        : 2,
@@ -78,12 +122,14 @@ const newSneaker = {
     qtd       : 1,
     unitPrice : 100_00
 };
-//_____________________________________________________________________________
-        
-addProductToTrolley(trolley, newShorts);
 
-trolley.printSummary()
+//_____________________________________________________________________________   
+// trolley.addProduct(newShorts);
 
-addProductToTrolley(trolley, newSneaker);
+// trolley.printSummary()
 
-trolley.printSummary()
+// trolley.addProduct(newSneaker);
+
+// trolley.printSummary()
+
+trolley.printDetails()
