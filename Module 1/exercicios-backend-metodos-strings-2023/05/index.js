b489c0e4231e86a0435619d336080f1281f17833
Rafael Pôrto <rafaelmportto@gmail.com>
2023-07-07 19:53:56 -0300

    :sparkles: feat: hide Credit Card informations

--- Module 1/exercicios-backend-metodos-strings-2023/05/index.js
+++ Module 1/exercicios-backend-metodos-strings-2023/05/index.js
@@ -1,1 +1,12 @@
+function hiddenInfo(creditCard){
+    const firstNumbers = creditCard.slice(0, 4);
+    const lastNumbers = creditCard.slice(-4);
+    const unseenInfo = firstNumbers + lastNumbers.padStart(12, "*");
+
+    return unseenInfo;
+}
+
+
 const numeroCartao = '1111222233334444';
+
+console.log(hiddenInfo(numeroCartao));
