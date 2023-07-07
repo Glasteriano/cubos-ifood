982a302e86e9cd06ba7498b8e00f4664e8d05c47
Rafael Pôrto <rafaelmportto@gmail.com>
2023-07-07 18:13:16 -0300

    :sparkles: feat: check two specific words

--- Module 1/exercicios-backend-metodos-strings-2023/01/index.js
+++ Module 1/exercicios-backend-metodos-strings-2023/01/index.js
@@ -1,6 +1,7 @@
 function checkComment(comment) {
     // put all string in lower-case to be easier to check
-    const forbiddenWord = comment.toLowerCase().includes("covid")
+    const lowerCaseSentence = comment.toLowerCase()
+    const forbiddenWord = lowerCaseSentence.includes("covid") || lowerCaseSentence.includes("pandemic");
 
     const verification = (!forbiddenWord) ? "Comment Granted" : "Comment blocked for the sake of forbidden words";
 
@@ -10,12 +11,14 @@ function checkComment(comment) {
 
 const comment1 = "This Covid is too dangerous!";
 const comment2 = "This COVID is too dangerous!";
-const comment3 = "This covid is too dangerous!";
-const comment4 = "This CoViD is too dangerous!";
-const comment5 = "This disease is too dangerous!";
+const comment3 = "This pandemic is too dangerous!";
+const comment4 = "This PaNdEmIc is too dangerous!";
+const comment5 = "This CoViD is too dangerous!";
+const comment6 = "This disease is too dangerous!";
 
 checkComment(comment1);
 checkComment(comment2);
 checkComment(comment3);
 checkComment(comment4);
 checkComment(comment5);
+checkComment(comment6);
