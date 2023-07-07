ca591fb3f9ee938352fba99cf78c8403ace259b8
Rafael Pôrto <rafaelmportto@gmail.com>
2023-07-07 18:26:30 -0300

    :sparkles: feat: become a URL more friendly

--- Module 1/exercicios-backend-metodos-strings-2023/03/index.js
+++ Module 1/exercicios-backend-metodos-strings-2023/03/index.js
@@ -1,1 +1,15 @@
+function friendlyURL(url){
+    let modifiedUrl = url.toLowerCase();
+
+    while (modifiedUrl.includes(" ")){
+        modifiedUrl = modifiedUrl.replace(" ", "-");
+    }
+
+    return modifiedUrl;
+};
+//_____________________________________________________________________________
+
 const texto = "Aprenda programar do zero na Cubos Academy";
+//_____________________________________________________________________________
+
+console.log(friendlyURL(texto));
