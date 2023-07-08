71fb2bef7f5d1cf1bfbed375d13a13947eb81521
Rafael Pôrto <rafaelmportto@gmail.com>
2023-07-08 05:31:08 -0300

    :sparkles: feat: change the queue

--- Module 1/exercicios-backend-metodos-arrays-2023/04/scripts.js
+++ Module 1/exercicios-backend-metodos-arrays-2023/04/scripts.js
@@ -1,1 +1,22 @@
+function queueConfig(patientArray, operarion, patientName){
+    const isSchedule = operarion.toLowerCase() === "schedule";
+    const isNext = operarion.toLowerCase() === "next";
+    //===============================================================
+
+    if (isSchedule){
+        patientArray.push(patientName);
+    };
+    //===============================================================
+
+    if(isNext){
+        patientArray.splice(0, 1);
+    };
+    //===============================================================
+    
+    return patientArray.join(", ");
+}
+
 const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+
+console.log(queueConfig(pacientes, "schedule", "Ígor"));
+console.log(queueConfig(pacientes, "next", "José"));
