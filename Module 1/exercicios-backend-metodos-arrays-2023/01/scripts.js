9fdae33a5984ed3aa535cb3bc4a210aee071d24e
Rafael Pôrto <rafaelmportto@gmail.com>
2023-07-08 04:23:35 -0300

    :white_check_mark: test: test basic methods on arrays

---
+++ Module 1/exercicios-backend-metodos-arrays-2023/01/scripts.js
@@ -0,0 +1,15 @@
+const frutas = ['Banana', 'Maçã', 'Abacaxi', 'Pêra', 'Uva'];
+
+frutas.reverse();
+
+const newString = frutas.join(", ");
+
+console.log(newString);
+//_____________________________________________________________________________
+
+frutas.shift();
+frutas.pop()
+frutas.reverse();
+frutas.push("Melão");
+
+console.log(frutas);
