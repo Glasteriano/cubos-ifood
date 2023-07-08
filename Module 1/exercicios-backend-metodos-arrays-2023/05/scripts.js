ea442e7127ec0e942f20c0a9493212ef22ae24b9
Rafael Pôrto <rafaelmportto@gmail.com>
2023-07-08 17:15:16 -0300

    :sparkles: feat: add new functions for the queue

--- Module 1/exercicios-backend-metodos-arrays-2023/05/scripts.js
+++ Module 1/exercicios-backend-metodos-arrays-2023/05/scripts.js
@@ -1,1 +1,44 @@
+function schedulePatient(patitentArray, patientName){
+    patitentArray.push(patientName);
+
+    console.log(patitentArray.join(", "));
+};
+//==============================================================
+
+function nextPatient(patientArray, patientName){
+    const isRightPatient = patientName === patientArray[0];
+
+    if (isRightPatient){
+        patientArray.shift();
+    };
+
+    console.log((isRightPatient) ? patientArray.join(", ") : "Wrong name for the first patient in the queue!");
+};
+//==============================================================
+
+function cancelPatient(patientArray, patientName){
+    const isInQueue = patientArray.includes(patientName);
+
+    if (isInQueue){
+        const indexOfPatient = patientArray.indexOf(patientName);
+        patientArray.splice(indexOfPatient, 1)
+    };
+    
+    console.log((isInQueue) ? patientArray.join(", ") : "Patient not in queue yet!");
+};
+//_____________________________________________________________________________
+
 const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+//_____________________________________________________________________________
+
+schedulePatient(pacientes, "Fernando");
+console.log();
+//------------------------------------------
+
+nextPatient(pacientes, "José");
+nextPatient(pacientes, "Marcelo");
+console.log();
+//------------------------------------------
+
+cancelPatient(pacientes, "João");
+cancelPatient(pacientes, "André");
