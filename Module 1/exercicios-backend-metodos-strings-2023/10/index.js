33b4682cc3eefd2b4058c9e92ecea65bed8b1fdc
Rafael Pôrto <rafaelmportto@gmail.com>
2023-07-13 05:39:46 -0300

    :sparkles: feat: check file

--- Module 1/exercicios-backend-metodos-strings-2023/10/index.js
+++ Module 1/exercicios-backend-metodos-strings-2023/10/index.js
@@ -1,1 +1,20 @@
+function checkExtension(file){
+    const isJpg = file.includes(".jpg");
+    const isJpeg = file.includes(".jpeg");
+    const isGif = file.includes(".gif");
+    const isPng = file.includes(".png");
+
+    const isValid = isJpg || isJpeg || isGif || isPng;
+
+    return (isValid) ? "Valid file" : "Invalid file";
+};
+
+
+//___________________________________________________________________
+//___________________________________________________________________
+
 const nomeArquivo = 'Foto da Familia.pdf';
+//___________________________________________________________________
+//___________________________________________________________________
+
+console.log(checkExtension(nomeArquivo));
