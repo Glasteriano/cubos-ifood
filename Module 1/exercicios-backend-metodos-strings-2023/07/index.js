8368da731114b875eb18ae5ae5b44c1cba27b2c8
Rafael Pôrto <rafaelmportto@gmail.com>
2023-07-13 04:19:37 -0300

    :sparkles: feat: ckeck e-mail

--- Module 1/exercicios-backend-metodos-strings-2023/07/index.js
+++ Module 1/exercicios-backend-metodos-strings-2023/07/index.js
@@ -1,1 +1,43 @@
-const email = "aluno@cubos.academy";
+function checkEmail(email){
+    const hasAt  = email.includes("@");
+    const hasDot = email.includes(".");
+
+    const dotIsNotTheFirst = email.indexOf(".") !== 0;
+    const dotIsNotTheLast  = email.lastIndexOf(".") !== (email.length - 1);
+
+    const isValidEmail = hasAt && hasDot && dotIsNotTheFirst && dotIsNotTheLast;
+
+    return (isValidEmail) ? "Valid e-mail" : "Invalid e-mail";
+};
+//___________________________________________________________________
+//___________________________________________________________________
+
+const email   = "aluno@cubos.academy";
+const email2  = "jose@cubos.academy.br";
+const email3  = "jose.messias@cubos.academy";
+const email4  = "jose.messias@cubos.io";
+const email5  = "jose@cubos";
+const email6  = "jose.messias@cubos";
+const email7  = "jose.messias@.";
+const email8  = "jose.@cubos";
+const email9  = ".@";
+const email10 = "@.";
+const email11 = "jose.messias@cubos.";
+const email12 = ".messias@cubos.";
+const email13 = ".messias@cubos";
+//___________________________________________________________________
+//___________________________________________________________________
+
+console.log(checkEmail(email));
+console.log(checkEmail(email2));
+console.log(checkEmail(email3));
+console.log(checkEmail(email4));
+console.log(checkEmail(email5));
+console.log(checkEmail(email6));
+console.log(checkEmail(email7));
+console.log(checkEmail(email8));
+console.log(checkEmail(email9));
+console.log(checkEmail(email10));
+console.log(checkEmail(email11));
+console.log(checkEmail(email12));
+console.log(checkEmail(email13));
