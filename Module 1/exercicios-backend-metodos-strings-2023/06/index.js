3a1346d5c83fc5329d8e6aa96453bfedf3548238
Rafael Pôrto <rafaelmportto@gmail.com>
2023-07-13 04:00:53 -0300

    :sparkles: feat: fix the mobile number

--- Module 1/exercicios-backend-metodos-strings-2023/06/index.js
+++ Module 1/exercicios-backend-metodos-strings-2023/06/index.js
@@ -1,1 +1,29 @@
+function fixNumber(number){
+    // put in string to be easier to change
+    const numberToString = number.toString()
+    const numberSize = numberToString.length;
+    //===================================================================================
+
+    if (numberSize === 8){
+        return `9 ${numberToString.slice(0, 4)}-${numberToString.slice(4)}`
+    };
+
+    if (numberSize === 9){
+        return `9 ${numberToString.slice(1, 5)}-${numberToString.slice(5)}`
+    };
+
+    if (numberSize === 10){
+        return `(${numberToString.slice(0, 2)}) 9 ${numberToString.slice(2, 6)}-${numberToString.slice(6)}`
+    };
+    //===================================================================================
+
+    return `(${numberToString.slice(0, 2)}) 9 ${numberToString.slice(3, 7)}-${numberToString.slice(7)}`;
+};
+//___________________________________________________________________
+//___________________________________________________________________
+
 const celular = 7199918888;
+//___________________________________________________________________
+//___________________________________________________________________
+
+console.log(fixNumber(celular))
