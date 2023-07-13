741c8368240c46323255e96dce7b2cac1ff8a9f3
Rafael Pôrto <rafaelmportto@gmail.com>
2023-07-13 05:28:14 -0300

    :sparkles: feat: creat a nickname

--- Module 1/exercicios-backend-metodos-strings-2023/09/index.js
+++ Module 1/exercicios-backend-metodos-strings-2023/09/index.js
@@ -1,1 +1,23 @@
+function nickName(name){
+    let newNick = "@";
+
+    for (let letter of name){
+        if (letter !== " "){
+            newNick += letter.toLowerCase();
+        };
+
+        if (newNick.length >= 13){
+            return newNick;
+        };
+    };
+
+    return newNick;
+}
+//___________________________________________________________________
+//___________________________________________________________________
+
 const nome = 'Guido Cerqueira';
+//___________________________________________________________________
+//___________________________________________________________________
+
+console.log(nickName(nome));
