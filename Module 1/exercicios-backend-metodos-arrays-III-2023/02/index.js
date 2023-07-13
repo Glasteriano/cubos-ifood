46c2bb19fc0ee49632f6116ea6d27a82a049db7b
Rafael Pôrto <rafaelmportto@gmail.com>
2023-07-13 01:57:20 -0300

    sparkles: feat: return the biggest name city

---
+++ Module 1/exercicios-backend-metodos-arrays-III-2023/02/index.js
@@ -0,0 +1,26 @@
+function cityByLength(stringArray){
+    
+    console.log(stringArray.reduce((acumulador, valorAtual) => {
+        let maiorPalavra = acumulador;
+
+        if (valorAtual.length > maiorPalavra.length){
+            maiorPalavra = valorAtual
+        };
+
+        return maiorPalavra;
+    }));
+};
+
+//___________________________________________________________________
+//___________________________________________________________________
+const cidades = [
+    "Salvador",
+    "São Paulo",
+    "Brasilia",
+    "Recife",
+    "Rio de Janeiro",
+];
+//___________________________________________________________________
+//___________________________________________________________________
+
+cityByLength(cidades);
