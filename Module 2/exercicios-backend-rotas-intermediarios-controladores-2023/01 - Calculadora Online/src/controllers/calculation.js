266f74fc4608a7d60ca2573190dddb20d16e181f
Rafael Pôrto <rafaelmportto@gmail.com>
2023-07-21 01:54:55 -0300

    :books: docs: add new folder

---
+++ Module 2/exercicios-backend-rotas-intermediarios-controladores-2023/01 - Calculadora Online/src/controllers/calculation.js
@@ -0,0 +1,21 @@
+function sum(req, res){
+
+};
+//================================================
+
+function subtraction(req, res){
+
+};
+//================================================
+
+function division(req, res){
+
+};
+//================================================
+
+function multiplication(req, res){
+
+};
+//================================================
+
+module.exports = {subtraction, sum, division, multiplication};
