dc362ba7f79bb1a3c3da7551fef9aacf2a05360c
Rafael Pôrto <rafaelmportto@gmail.com>
2023-07-21 02:26:14 -0300

    :sparkles: feat: math calc

--- Module 2/exercicios-backend-rotas-intermediarios-controladores-2023/01 - Calculadora Online/src/controllers/calculation.js
+++ Module 2/exercicios-backend-rotas-intermediarios-controladores-2023/01 - Calculadora Online/src/controllers/calculation.js
@@ -1,21 +1,33 @@
 function sum(req, res){
+    const {num1, num2} = req.query;
+    let total          = (~~num1) + (~~num2);  // ~~ convert to integer
 
+    res.send(`${total}`);  // res.send() only accept strings, tht's why I put in string format
 };
-//================================================
+//====================================================
 
 function subtraction(req, res){
+    const {num1, num2} = req.query;
+    let total          = (~~num1) - (~~num2);
 
+    res.send(`${total}`);
 };
-//================================================
+//====================================================
 
 function division(req, res){
+    const {num1, num2} = req.query;
+    let total          = (~~num1) / (~~num2);
 
+    res.send(`${total}`);
 };
-//================================================
+//====================================================
 
 function multiplication(req, res){
+    const {num1, num2} = req.query;
+    let total          = (~~num1) * (~~num2);
 
+    res.send(`${total}`);
 };
-//================================================
+//====================================================
 
 module.exports = {subtraction, sum, division, multiplication};
--- Module 2/exercicios-backend-rotas-intermediarios-controladores-2023/01 - Calculadora Online/src/index.js
+++ Module 2/exercicios-backend-rotas-intermediarios-controladores-2023/01 - Calculadora Online/src/index.js
@@ -1,17 +1,18 @@
 const {sum, division, multiplication, subtraction} = require("./controllers/calculation");
+
 const express = require("express");
-const app = express();
+const app     = express();
+
 const port = 3_000;
+//=================================================================================================
 
 app.get("/", (req, res) => {
     res.send("Welcome to my script");
 });
-
 app.get("/sum", sum);
 app.get("/division", division);
 app.get("/subtraction", subtraction);
 app.get("/multiplication", multiplication);
-
-
+//=================================================================================================
 
 app.listen(port, () => console.log(`\nListening on Port: ${port}\n`))
