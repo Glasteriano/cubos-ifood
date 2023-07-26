dfbccfe7bb1b3d5a82f95c21b58d12664a9b1fd3
Rafael Pôrto <rafaelmportto@gmail.com>
2023-07-26 07:14:46 -0300

    :sparkles: feat: get a book by id

--- Module 2/exercicios-backend-api-rest-2023/03 - Biblioteca On-line/src/controllers/library.js
+++ Module 2/exercicios-backend-api-rest-2023/03 - Biblioteca On-line/src/controllers/library.js
@@ -1,14 +1,34 @@
-const dataBase = require("../dataBase/dataBase");
+const booksDataBase = require("../dataBase/dataBase");
 //========================================================================
 //========================================================================
 
 //////////---------- Get ----------//////////
 function showBook(_, res){
-    return res.status(200).json(dataBase);
+    return res.status(200).json(booksDataBase);
+};
+//____________________________________________________
+
+function getBookById(req, res){
+    const { id } = req.params;
+
+    if (!Number(id)){
+        return res.status(400).json({"message" : "The id might be a number."});
+    };
+    //--------------------------------------
+
+    for (let book of booksDataBase){
+        if (book.id === Number(id)){
+            return res.status(200).json(book)
+        };
+    };
+    //--------------------------------------
+    
+    return res.status(404).json({"message" : "This id does not exist!"});
 };
 //========================================================================
 //========================================================================
 
 module.exports = {
-    showBook
+    showBook,
+    getBookById
 };
