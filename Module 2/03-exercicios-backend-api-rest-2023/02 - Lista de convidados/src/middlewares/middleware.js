5b76c7e1870fe869cca17d124669a78e86063058
Rafael Pôrto <rafaelmportto@gmail.com>
2023-08-12 22:03:46 -0300

    �️�️�️�️�️:car: database: solve some tasks

--- Module 2/03-exercicios-backend-api-rest-2023/02 - Lista de convidados/src/middlewares/middleware.js
+++ Module 2/03-exercicios-backend-api-rest-2023/02 - Lista de convidados/src/middlewares/middleware.js
@@ -1,18 +1,18 @@
 const guestRouter = require("../controllers/guest");
 //===================================================================
 
-function nameQuery(req, res, next){  // Middleware that verify if the user send a query
+function nameQuery(req, res, next) {  // Middleware that verify if the user send a query
     const { name } = req.query;
     //___________________________________________
 
-    if (!name){  // localhost:3000/guest
+    if (!name) {  // localhost:3000/guest
         guestRouter.showGuests(req, res);
     }
     else if (name) {  // localhost:3000/guest?name=theNameHere
         guestRouter.getGuest(req, res);
     }
     else {  // should not enter in this else condition
-        return res.status(500).json({"message" : "Unexpected error"})
+        return res.status(500).json({ "message": "Unexpected error" })
     };
     //___________________________________________
 
